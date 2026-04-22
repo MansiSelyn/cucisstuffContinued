8562d954d91b72cfd510b22579e061abe5e3b4e8
martinman1991 <98027494+martinman1991@users.noreply.github.com>
2026-04-22 14:44:39 +0200

    Add files via upload

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');          // XAMPP alapértelmezett
-define('DB_PASS', '');              // üres jelszó
-define('DB_NAME', 'cuci_ady_pepa_hu'); // a db.sql-ben szereplő adatbázisnév
+define('DB_USER', 'cuci_ady_pepa_hu_usr');
+define('DB_PASS', 'utQTkTN2Q5WD7zal8IBFmQ');
+define('DB_NAME', 'cuci_ady_pepa_hu');
